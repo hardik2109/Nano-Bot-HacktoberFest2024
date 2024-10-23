2d130646914b692fb7fdf1aad8caf0495692a8eb
sudhanshu tiwary <116373262+blackmamba1231@users.noreply.github.com>
2024-10-24 01:29:26 +0530

    Bubble sort

---
+++ 1_bubbleSort.java
@@ -0,0 +1,16 @@
+public class 1_bubbleSort {
+    public static void main(String args[]){
+        int arr[]= {7,8,9,3,1,2};
+        //bubble sort TC= (n^2)
+        for(int i =0;i<arr.length()-1;i++){
+            for(int j=0;j<arr.length()-i-1;j++){
+              if(arr[j]>arr[j+1]){
+                //swap
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+              }
+            }
+        }
+    }
+}
